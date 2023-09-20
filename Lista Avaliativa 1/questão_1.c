488d37e3c1e39ed68441d17819005077dbf0fe43
Victor Davidsom <vctrdavidsomdev@gmail.com>
2023-09-19 21:06:03 -0300

    questão 1 ok

--- Lista Avaliativa 1/questão_1.c
+++ Lista Avaliativa 1/questão_1.c
@@ -3,19 +3,19 @@
 #include <locale.h>
 int main(){
     setlocale(LC_ALL, "Portuguese_Brazil");
-    int x;
+    int anoDePesquisa;
     printf("digite o ano para pesquisa: \n");
-    scanf("%i",&x);
-    if(x >= 1800 && x <= 2022 ){
-        if(x >= 1896 && x%4 == 0){
-            printf("Os Jogos Olímpicos de Verão ocorreram no ano de %i \n", x);
-        }else if(x >= 1930 && x%4 == 2){
-            printf("A Copa do Mundo de Futebol ocorreu no ano de %i \n", x);
+    scanf("%i",&anoDePesquisa);
+    if(anoDePesquisa >= 1800 && anoDePesquisa <= 2022 ){
+        if(anoDePesquisa >= 1896 && anoDePesquisa%4 == 0){
+            printf("Os Jogos Olímpicos de Verão ocorreram no ano de %i \n", anoDePesquisa);
+        }else if(anoDePesquisa >= 1930 && anoDePesquisa%4 == 2){
+            printf("A Copa do Mundo de Futebol ocorreu no ano de %i \n", anoDePesquisa);
         }else{
-            printf("Não houve Jogos Olímpicos de Verão ou Copa do Mundo no ano de %i \n", x);
+            printf("Não houve Jogos Olímpicos de Verão ou Copa do Mundo no ano de %i \n", anoDePesquisa);
         }
     }else{
-        printf("o ano %i não está disponivel para pesquisa \n",x);
+        printf("o ano %i não está disponivel para pesquisa \n",anoDePesquisa);
     }
     return 0;
 }
